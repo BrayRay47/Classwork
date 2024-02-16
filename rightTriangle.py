99eb929ba75e9f4e999e76409f4ee5e7158a4aa4
BrayRay47 <160148701+BrayRay47@users.noreply.github.com>
2024-02-16 10:42:44 -0600

    Update rightTriangle.py

--- rightTriangle.py
+++ rightTriangle.py
@@ -3,7 +3,7 @@ height = int(input("Please input a positive height: "))
 while height <= 0:
     height = int(input("Please input a positive height: "))
 
-counter = 1
+starCount = 1
 spaceCount = height
 
 print("Here is your art.")
@@ -11,9 +11,9 @@ print("Here is your art.")
 for rows in range(height):
     for spaces in range(spaceCount - 1):
         print(end = "  ")
-    for stars in range(counter):
+    for stars in range(starCount):
         print("*", end = " ")
     spaceCount = spaceCount - 1
-    counter = counter + 1
+    starCount = starCount + 1
 
     print()
